b3b1a27b7781ea76c7e3b5db7ecb8f275831913d
Yash James <jamesyash007@gmail.com>
2023-04-27 00:07:49 +0530

    New-items

---
+++ CSS-Practise/Countdown/Preparing.js
@@ -0,0 +1,6 @@
+// FOR HTML :
+<script src="https://kit.fontawesome.com/07a6501610.js" crossorigin="anonymous"></script>
+
+// FOR CSS :
+// @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&display=swap');
+
